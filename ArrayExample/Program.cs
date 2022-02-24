54f14824947d28117b87e33b8527e1d16a769497
M153428 <M153428@DELL603950>
2022-02-24 09:50:53 +0800

    Add project files.

---
+++ ArrayExample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Display number from 0 to 9
+            int[] myArray = { 1, 2, 3, 4, 5, 22, 45, 53 };
+            int arrLength = myArray.Length;
+
+            for (int i = 0; i < arrLength; i++)
+            {
+                Console.WriteLine("array item : " + myArray[i]);
+            }
+        }
+    }
+}
